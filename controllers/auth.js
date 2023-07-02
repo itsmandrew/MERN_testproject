27002853c316cb193aa70c8babb44fa2ed3828f6
Andrew Chang <changa17@uci.edu>
2023-07-02 14:12:21 -0700

    auth.js for routes + more in controllers + env

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,14 +29,35 @@ export const register = async (req, res) => {
             friends,
             location,
             occupation,
-            vieweedProfile: Math.floor(Math.random() * 10000),
+            viewedProfile: Math.floor(Math.random() * 10000),
             impressions:  Math.floor(Math.random() * 10000)
         });
 
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
-        
-    } catch (err) {
 
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+
+    }
+}
+
+/* LOGGING IN */
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({email: email });
+
+        if (!user) return res.status(400).json({ msg: "User does not exist. "});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. "});
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token , user });
+    } catch {
+        res.status(500).json({ error: err.message });
     }
 }
